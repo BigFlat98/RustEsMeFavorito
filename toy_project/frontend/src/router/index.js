cf93a93ecbfcc47e4f55a75cfa8f2b814dbd450a
BigFlat98 <hdj9836@naver.com>
2025-05-23 11:36:24 +0900

    스니팻 세팅팅

--- toy_project/frontend/src/router/index.js
+++ toy_project/frontend/src/router/index.js
@@ -14,7 +14,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/main',
+    name: 'main',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/MainView.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ toy_project/frontend/src/views/MainView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  name: '',
+  components: {},
+  setup() {
+    // 반응형 상태
+    const state = ref({});
+
+    // 계산된 속성
+    const computedProps = computed(() => {});
+
+    // 메서드
+    const methods = {};
+
+    // 생애주기 훅
+    // onMounted(() => {});
+    // onUnmounted(() => {});
+
+    return {
+      state,
+      computedProps,
+      methods
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
